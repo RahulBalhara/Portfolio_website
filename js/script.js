1eb1e945c1ce62a866b64c2876963d149da2d896
RahulBalhara <rahulbalhara821@gmail.com>
2025-04-06 16:06:31 +0530

    add style-switcher

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* ========= Typing Animation ========= */
+var typed = new Typed(".typing", {
+    strings: [
+        "a Front-End Developer.",
+        "a UI/UX Enthusiast.",
+        "learning React.js.",
+        "a Creative Thinker.",
+        "building cool websites."
+      ],
+      
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
+  
