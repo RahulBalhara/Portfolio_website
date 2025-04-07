bea28c2a1edf0b912c1ea8e30b703e6293aa6bd6
RahulBalhara <rahulbalhara821@gmail.com>
2025-04-07 21:24:16 +0530

    add features to home page and navbar

--- js/script.js
+++ js/script.js
@@ -12,19 +12,26 @@ var typed = new Typed(".typing", {
     backSpeed: 60,
     loop: true
   });
-  
-//   <script>
-//   // Get all nav links
-//   const navLinks = document.querySelectorAll(".nav li a");
+ 
+  const sections = document.querySelectorAll('section');
+  const navLinks = document.querySelectorAll('.nav li a');
 
-//   navLinks.forEach(link => {
-//     link.addEventListener("click", () => {
-//       // Remove active class from all links
-//       navLinks.forEach(nav => nav.classList.remove("active"));
-      
-//       // Add active class to the clicked one
-//       link.classList.add("active");
-//     });
-//   });
-// </script>
+  window.addEventListener('scroll', () => {
+    let current = '';
+
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop - 100;
+      const sectionHeight = section.clientHeight;
+      if (pageYOffset >= sectionTop && pageYOffset < sectionTop + sectionHeight) {
+        current = section.getAttribute('id');
+      }
+    });
+
+    navLinks.forEach(link => {
+      link.classList.remove('active');
+      if (link.getAttribute('href') === `#${current}`) {
+        link.classList.add('active');
+      }
+    });
+  });
 
